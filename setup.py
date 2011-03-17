7e4f85292a8aad0ed7a7d4f02318b0233fd476c8
Robin Jarry <robin.jarry@ablogix.fr>
2011-03-17 15:27:01 +0100

    version 0.1.0
    Signed-off-by: Robin Jarry <robin.jarry@ablogix.fr>

---
+++ setup.py
@@ -0,0 +1,29 @@
+# docutils-extensions's setup.py
+from distutils.core import setup
+setup(
+    name = "clearquest2rst",
+    version = "0.1.0",
+    license = "MIT",
+    requires = ["docutils (>=0.7)", "docutilsextensions (>=0.1.0)"],
+
+    description = "A docutils for converting ClearQuest requests to RST tables.",
+    long_description = open('README.rst').read(),
+    author = "Robin Jarry",
+    author_email = "robin.jarry@ablogix.fr",
+    url = "http://github.com/robin-jarry/clearquest2rst",
+    download_url = "http://github.com/robin-jarry/clearquest2rst",
+    keywords = ["docutils", "rst", "reStructuredText", "clearquest"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: Beta",
+        "Environment :: Windows",
+        'Intended Audience :: End Users/Desktop',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Document Generation",
+    ],
+    packages = ["docutilsextensions"],
+    package_dir = {'': 'src'}
+)
+
